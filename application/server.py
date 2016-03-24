94d4f4ff4eae1c1027d43686db766339cf10feda
Andrew Moore <mooreandrewlr@gmail.com>
2016-03-24 21:45:03 +0000

    Updated Change Api Stub

--- application/server.py
+++ application/server.py
@@ -3,7 +3,12 @@ import os, requests, json, datetime
 from flask import Flask, request, abort, redirect, url_for, Response, jsonify
 from random import randint
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/raise', methods = ['GET', 'POST'])
 def test():
     test = 'test'
     return Response(json.dumps({'rfc': str(randint(0,99999))}),  mimetype='application/json')
+
+
+@app.route('/check_service/<service>', methods = ['GET', 'POST'])
+def check_service(service):
+    return Response(json.dumps({'status': 'Ok'}),  mimetype='application/json')
